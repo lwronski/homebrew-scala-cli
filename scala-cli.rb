29ac2db962141738fb8f01e0d93c2081eb6b9427
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-07-26 15:33:26 +0200

    Create formula for scala-cli

---
+++ scala-cli.rb
@@ -0,0 +1,24 @@
+class ScalaCli < Formula
+  desc "Launcher for ScalaCli"
+  homepage "https://virtuslabrnd.github.io/scala-cli/"
+  url "https://github.com/VirtuslabRnD/scala-cli/releases/download/latest/scala-x86_64-apple-darwin.gz"
+  version "latest"
+  bottle :unneeded
+
+  depends_on java: "1.8+" if MacOS.version < :catalina
+  depends_on "openjdk" if MacOS.version >= :catalina
+
+  def install
+    bin.install "scala-x86_64-apple-darwin" => "scala-cli"
+  end
+
+  test do
+    (testpath/"Hello.scala").write "object Hello {
+                                def main(args: Array[String]): Unit =
+                                  println(\"Hello from Scala\")
+                              }"
+    output = shell_output("#{bin}/scala-cli Hello.scala")
+    assert_equal ["Hello from Scala\n"], output.lines
+
+  end
+end
